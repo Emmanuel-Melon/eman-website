c57cc6fe2456f3adbc90b9007bf4c7fbe9581e22
Emmanuel Gatwech <emmanuelgatwech@gmail.com>
2023-04-02 18:28:55 +0300

    Added personal info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Emmanuel Daniel</h1>
+<p>I like to craft solid and scalable frontend products with great user experiences.</p>
+<h3>Find me on</h3>
+<ul>
+  <li><a href="https://github.com/emmanuel-melon">GitHub</a></li>
+  <li><a href="https://www.linkedin.com/in/emmanuel-gatwech/">LinkedIn</a></li>
+  <li><a href="https://twitter.com/JunubiMan">Twitter</a></li>
+</ul>
