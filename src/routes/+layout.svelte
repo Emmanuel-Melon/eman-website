0adf5229b156c3ea417c3f2e0c553b475ea33a50
Eman <emmanuelgatwech@gmail.com>
2023-05-16 18:44:03 +0300

    feat: add basic content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,14 @@
 <script>
+
+// Your selected Skeleton theme:
+import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+// This contains the bulk of Skeletons required styles:
+// NOTE: this will be renamed skeleton.css in the v2.x release.
+import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+
 	import '../app.css';
 </script>
 
@@ -9,6 +19,17 @@
     content="Emmanuel Gatwech"
   />
 </svelte:head>
+<header>
+	<ul>
+		<li>About</li>
+		<li>Experience</li>
+		<li>Projects</li>
+		<li>Open Source</li>
+	</ul>
+</header>
 <main>
 	<slot />
 </main>
+<footer>
+	<p>Eman 2023</p>
+</footer>
